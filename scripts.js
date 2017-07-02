2b13fc46c340372d7e03e070359077ba57b7eefd
Gale Bonker <gbonker@andrew.cmu.edu>
2017-07-01 18:02:32 -0700

    initial commit with game logic

---
+++ scripts.js
@@ -0,0 +1,101 @@
+window.onload = function(){
+  var letter = "X";
+  var newGameButton = document.getElementById("new-game");
+  newGameButton.addEventListener("click", function() {
+    reset();
+  });
+
+  $(".board-space").click(function(){
+      
+    // place move
+    if (!gameOver() && this.innerText === "") {
+      $(this).text(letter);
+      switchLetter();
+    }
+    
+    // after move is placed, see if the game is over
+    if (gameOver()) {
+      if (someoneWon()) {
+        $("#who-won").text("Game Over! " + letter + " won!");
+      } else if (noOneWon()) {
+        $("#who-won").text("Game Over! All spaces are filled, but no one won.");
+      }
+      return;
+    }
+
+  }); 
+
+
+
+  function switchLetter() {
+    if (!gameOver()) {
+      if (letter === "X") {
+        letter = "O";
+      } else {
+        letter = "X";
+      }
+    }
+  }
+
+  function areEqual(a,b,c){
+    return (a === b && b === c && a !== "");
+  }
+  
+  function reset() {
+    $("#top-left").text("");
+    $("#top-middle").text("");
+    $("#top-right").text("");
+    $("#middle-left").text("");
+    $("#middle-middle").text("");
+    $("#middle-right").text("");
+    $("#bottom-left").text("");
+    $("#bottom-middle").text("");
+    $("#bottom-right").text("");
+    $("#who-won").text("");
+  }
+
+  function someoneWon() {
+    var topLeftLetter = $("#top-left").text();
+    var topMiddleLetter = $("#top-middle").text();
+    var topRightLetter = $("#top-right").text();
+    var middleLeftLetter = $("#middle-left").text();
+    var middleMiddleLetter = $("#middle-middle").text();
+    var middleRightLetter = $("#middle-right").text();
+    var bottomLeftLetter = $("#bottom-left").text();
+    var bottomMiddleLetter = $("#bottom-middle").text();
+    var bottomRightLetter = $("#bottom-right").text();
+    
+    return ( 
+      // horizontal 3 in a row
+      areEqual(topLeftLetter, topMiddleLetter, topRightLetter) ||
+      areEqual(middleLeftLetter, middleMiddleLetter, middleRightLetter) ||
+      areEqual(bottomLeftLetter, bottomMiddleLetter, bottomRightLetter) ||
+      // vertical 3 in a row
+      areEqual(topLeftLetter, middleLeftLetter, bottomLeftLetter) ||
+      areEqual(topMiddleLetter, middleMiddleLetter, bottomMiddleLetter) ||
+      areEqual(topRightLetter, middleRightLetter, bottomRightLetter) ||
+      // diagonal 3 in a row
+      areEqual(topLeftLetter, middleMiddleLetter, bottomRightLetter) ||
+      areEqual(topRightLetter, middleMiddleLetter, bottomLeftLetter)
+    );
+  } 
+
+  function allSpacesFilled() {
+    var boardSpaces = document.getElementsByClassName("board-space");
+    for (var i = 0; i < boardSpaces.length; i++) {
+      if (boardSpaces[i].innerText === "") {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function noOneWon() {
+    return allSpacesFilled() && !someoneWon();
+  }
+
+  function gameOver() {
+    return noOneWon() || someoneWon();
+  }
+
+}
